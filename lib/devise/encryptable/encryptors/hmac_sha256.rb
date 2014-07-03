922d0b98614911e8090822d956e5731aaf9fde9a
Yuki Okubo <y-okubo@nekojarashi.com>
2014-07-03 17:53:36 +0900

    Add encryptor

---
+++ lib/devise/encryptable/encryptors/hmac_sha256.rb
@@ -0,0 +1,15 @@
+require 'openssl'
+require 'Base64'
+
+module Devise
+  module Encryptable
+    module Encryptors
+      class HmacSha256 < Base
+        def self.digest(password, stretches, salt, pepper)
+          str = [password].flatten.compact.join
+          Base64.encode64(OpenSSL::HMAC.digest(OpenSSL::Digest.new('sha256'), 'secret-key', str)).strip()
+        end
+      end
+    end
+  end
+end
